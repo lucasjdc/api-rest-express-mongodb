5b4381c627ac3d5d05762ddf0866de238b0b0f22
lucasjdc <lucasjpdc@gmail.com>
2025-05-27 23:07:52 -0300

    Criando o servidor

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 'use strict';
 import http from "http";
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"Content-Type": "text/plain"});
+    res.end("Curso de Node.js")
+});
+
+server.listen(PORT, () =>{
+console.log(`[INFO] Server is online at http://localhost:${PORT}`);
+});
