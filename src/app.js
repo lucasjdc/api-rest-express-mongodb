ba1ffcaaac6a26b8d38be03b7fd6ddfff4cf39f0
lucasjdc <lucasjpdc@gmail.com>
2025-05-28 14:34:48 -0300

    Criando registros com POST

--- src/app.js
+++ src/app.js
@@ -2,9 +2,30 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        titulo: "Aplicações web real-time com Node.js"
+    },
+    {
+        id: 2,
+        titulo: "Meteor: Criando aplicações web real-time com JavaScript"
+    }
+]
 
 app.get("/",(req, res)=>{
     res.status(200).send("Curso de Node.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadadstrado com sucesso");
 });
 
 export default app;
