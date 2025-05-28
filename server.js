7db34f918946a82ebd270d5367142109e5ed1bab
lucasjdc <lucasjpdc@gmail.com>
2025-05-27 23:44:17 -0300

    Iniciando com Express

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 'use strict';
-import http from "http";
+
+import app from "./src/app.js";
 
 const PORT = 3000;
 
-const rotas = {
-    "/":"Curso de Express API",
-    "/livros":"Entrei na rota livros",
-    "/autores":"Entrei na rota autores" 
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end(rotas[req.url]);
-});
-
-server.listen(PORT, () =>{
+app.listen(PORT, () =>{
 console.log(`[INFO] Server is online at http://localhost:${PORT}`);
-
 });
---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict';
+import express from "express";
+
+const app = express();
+
+app.get("/",(req, res)=>{
+    res.status(200).send("Curso de Node.js");
+});
+
+export default app;
