d40985088e76ecdae3f9fde93874c5b1e9c5f2a7
lucasjdc <lucasjpdc@gmail.com>
2025-05-28 19:00:30 -0300

    Deletando livros

--- src/app.js
+++ src/app.js
@@ -45,4 +45,10 @@ app.put("/livros/:id", (req, res) => {
     res.status(200).json(livros[index]);
 });
 
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso.");
+});
+
 export default app;
