8802f70760dbcd8b79eea436fb51bd1c969a7217
lucasjdc <lucasjpdc@gmail.com>
2025-05-30 17:10:31 -0300

    Erros de validação

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -2,10 +2,19 @@
 import mongoose from "mongoose";
 import { autorSchema } from "./Autor.js";
 
+const frase = "do livro é obrigatório";
+
 const livroSchema = new mongoose.Schema({
-    titulo: { type: String, required: true },
+    
+    titulo: { 
+        type: String,
+        required: [true, "O título " + frase] 
+    },
     autor: autorSchema,
-    editora: { type: String }
+    editora: {
+        type: String,
+        required: [true, "A editora " + frase]
+    }
 }, { versionKey: false });
 
 const Livro = mongoose.model("Livro", livroSchema);
